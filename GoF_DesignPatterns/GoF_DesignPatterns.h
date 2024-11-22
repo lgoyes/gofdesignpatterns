c049279a98a1c4ccf32e1a6769d5d320b487872d
Luis David Goyes Garces <luis.goyes117@gmail.com>
2024-11-22 15:29:02 -0500

    Initial Commit

---
+++ GoF_DesignPatterns/GoF_DesignPatterns.h
@@ -0,0 +1,18 @@
+//
+//  GoF_DesignPatterns.h
+//  GoF_DesignPatterns
+//
+//  Created by Luis David Goyes Garces on 22/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoF_DesignPatterns.
+FOUNDATION_EXPORT double GoF_DesignPatternsVersionNumber;
+
+//! Project version string for GoF_DesignPatterns.
+FOUNDATION_EXPORT const unsigned char GoF_DesignPatternsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoF_DesignPatterns/PublicHeader.h>
+
+
