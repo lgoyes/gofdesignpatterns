518d14528a3de4e24fc91bb955067391487af9bf
Luis David Goyes Garces <luis.goyes117@gmail.com>
2025-04-29 17:40:51 -0500

    Prototype

---
+++ GoF_DesignPatterns/Prototype/PrototypePattern.swift
@@ -0,0 +1,79 @@
+//
+//  PrototypePattern.swift
+//  GoF_DesignPatterns
+//
+//  Created by Luis David Goyes Garces on 29/4/25.
+//
+
+class CarEngine {
+    private let type: String
+    private let displacement: Int
+    private let power: Int
+    private let torque: Int
+    private let efficiency: Double
+    private let year: Int
+    init(type: String, displacement: Int, power: Int, torque: Int, efficiency: Double, year: Int) {
+        self.type = type
+        self.displacement = displacement
+        self.power = power
+        self.torque = torque
+        self.efficiency = efficiency
+        self.year = year
+    }
+    
+    // other methods
+}
+
+protocol Prototype {
+    func clone() -> Self
+}
+
+class Car: Prototype {
+    private let brand: String
+    private let color: String
+    private let engine: CarEngine
+    private let transmission: String
+    private let seats: Int
+    private let doors: Int
+    private let maxSpeed: Int
+    private let fuelType: String
+    private let price: Double
+    var year: Int
+    
+    init(brand: String, color: String, engine: CarEngine, transmission: String, seats: Int, doors: Int, maxSpeed: Int, fuelType: String, price: Double, year: Int) {
+        self.brand = brand
+        self.color = color
+        self.engine = engine
+        self.transmission = transmission
+        self.seats = seats
+        self.doors = doors
+        self.maxSpeed = maxSpeed
+        self.fuelType = fuelType
+        self.price = price
+        self.year = year
+    }
+    
+    required init(copyFrom prototype: Car) {
+        self.brand = prototype.brand
+        self.color = prototype.color
+        self.engine = prototype.engine
+        self.transmission = prototype.transmission
+        self.seats = prototype.seats
+        self.doors = prototype.doors
+        self.maxSpeed = prototype.maxSpeed
+        self.fuelType = prototype.fuelType
+        self.price = prototype.price
+        self.year = prototype.year
+    }
+    
+    func clone() -> Self {
+        return Self.init(copyFrom: self)
+    }
+    // other methods
+}
+
+class CarSubclass: Car {
+    override func clone() -> Self /* CarSubclass */ {
+        super.clone() // Retorna "Car()"
+    }
+}
---
+++ GoF_DesignPatternsTests/PrototypeTests.swift
@@ -0,0 +1,22 @@
+//
+//  PrototypeTests.swift
+//  GoF_DesignPatterns
+//
+//  Created by Luis David Goyes Garces on 29/4/25.
+//
+
+import Testing
+@testable import GoF_DesignPatterns
+
+class PrototypeTests {
+    @Test
+    func invalidCopyFromReferenceType() {
+        let engine = CarEngine(type: "V8", displacement: 6000, power: 500, torque: 400, efficiency: 0.8, year: 2020)
+        let referenceCar = Car(brand: "Toyota", color: "White", engine: engine, transmission: "Automatic", seats: 5, doors: 4, maxSpeed: 200, fuelType: "Gasoline", price: 20000, year: 2020)
+        
+        let newCar = referenceCar.clone()
+        newCar.year = 2025
+        
+        #expect(referenceCar.year == 2020)
+    }
+}
